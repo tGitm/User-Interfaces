5954cf01e48a7b87675dd943aa14abcfb41951a8
tGitm <timrus99@gmail.com>
2021-04-10 22:31:57 +0200

    Initial commit

---
+++ Domača-naloga3/src/dn03/Controller.java
@@ -0,0 +1,18 @@
+package dn03;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
+public class Controller {
+    public MenuItem aboutPCB;
+    public MenuItem aboutACB;
+
+    public void pobrisiCB(ActionEvent actionEvent) {
+    }
+
+    public void shraniCB(ActionEvent actionEvent) {
+    }
+
+    public void exitCB(ActionEvent actionEvent) {
+    }
+}
