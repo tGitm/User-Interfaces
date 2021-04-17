867310b761ade0b27b7225118c26adbf14d70f1a
tGitm <timrus99@gmail.com>
2021-04-17 16:33:03 +0200

    DN04 Design completed

---
+++ Domača-naloga4/src/dn04/Controller.java
@@ -0,0 +1,42 @@
+package dn04;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class Controller {
+    public DatePicker zacetekIzposoje;
+    public DatePicker konecIzposoje;
+    public ComboBox krajPrevzema;
+    public ComboBox krajVrnitve;
+    public ComboBox tipVozila;
+    public ComboBox tipMenjalnika;
+    public ComboBox gorivo;
+    public TextField uraRez;
+    public TextField uraVrnit;
+    public TextField ime;
+    public TextField naslovStranke;
+    public TextField emailStranke;
+    public TextField telStevilka;
+    public TextField starostStranke;
+    public TextField priimek;
+    public TextField postnaStevilka;
+    public TextField izkusnje;
+    public TextField zaPlacilo;
+    public TextField stKartice;
+    public TextField ccvKoda;
+    public RadioButton kartica;
+    public RadioButton gotovina;
+    public Label status;
+
+    public void ponastaviCB(ActionEvent actionEvent) {
+    }
+
+    public void shraniCB(ActionEvent actionEvent) {
+    }
+
+    public void zapriCB(ActionEvent actionEvent) {
+    }
+
+    public void aboutCB(ActionEvent actionEvent) {
+    }
+}
